6109bdf46e9cfae6c8ac3f7a36efe121ac7aedf1
Filipe França <dev.filipefranca@gmail.com>
2026-06-11 13:36:36 -0300

    Transferencia e consulta realizadas

--- banco_system.c
+++ banco_system.c
@@ -11,7 +11,7 @@ int main (void) {
 
     typedef struct {
         transacao tipo;
-        int valor;
+        double valor;
         char destino[51];
 
     } historico;
@@ -19,10 +19,28 @@ int main (void) {
     typedef struct {
         char nome[51];
         double saldo;
-        historico extrato;
+        historico extrato[999];
 
     } user;
+
+
+    user one;
+    one.saldo = 0;
     
+    printf("=================\n");
+    printf("   BEM VINDO\n");
+    printf("=================\n");
+    printf("<JARVIS>: olá me chamo Jarvis, serei seu auxílio,\n");
+    printf("          vamos começar criando seu usuário, di-\n");
+    printf("          te seu nome:\n");
+    printf("          USERNAME:");
+    fgets(one.nome, sizeof(one.nome), stdin);
+    one.nome[strcspn(one.nome, "\n")] = '\0';
+    printf("\n");
+    printf("<JARVIS>: Perfeito senhor(a) %s; estou contente em\n", one.nome);
+    printf("          te ter por aqui, seja bem vindo!\n");
+    printf("\n");
+
     typedef enum {
         CONSULTAR_S = 1,
         DEPOSITAR,
@@ -32,6 +50,7 @@ int main (void) {
     } menu;
 
     int opcao;
+    int id_transacao = 0;
 
     do
     {
@@ -48,16 +67,48 @@ int main (void) {
         printf("5- sair\n");
         printf("Digite sua opcao:");
         scanf("%i", &opcao);
+        while (getchar() != '\n');
         switch (opcao)
         {
         case CONSULTAR_S:
-            
+            printf("\n");
+            printf("<JARVIS>: Seu saldo atual: %.2lf\n", one.saldo);
+            printf("\n");
+            break;
+        case TRANSFERIR:
+            printf("\n");
+            printf("<JARVIS>: informe o nome da pessoa a se\n");
+            printf("          transferir\n");
+            printf("          DESTINO:");
+            fgets(one.extrato[id_transacao].destino, sizeof(one.extrato[id_transacao].destino), stdin);
+            one.extrato[id_transacao].destino[strcspn(one.extrato[id_transacao].destino, "\n")] = '\0';
+            printf("\n");
+            printf("<JARVIS>: informe o valor a ser trans-\n");
+            printf("          ferido\n");
+            printf("          VALOR:");
+            scanf("%lf", &one.extrato[id_transacao].valor);
+            if (one.extrato[id_transacao].valor <= one.saldo) {
+                printf("\n");
+                printf("<JARVIS>: TRANSFERENCIA REALIZADA\n");
+                one.saldo -= one.extrato[id_transacao].valor;
+            }
+            else {
+                printf("\n");
+                printf("<JARVIS>: VALOR INSUFICIENTE\n");
+                one.extrato[id_transacao].valor = 0;
+            }
+            one.extrato->tipo = TRANSFERENCIA;
+            id_transacao ++;
+            printf("\n");
             break;
         default:
+            printf("\n");
+            printf("OPCAO INVALIDA\n");
+            printf("\n");
             break;
         }
 
-    } while (opcao > 5 || opcao < 1);
+    } while (opcao != SAIR);
 
     
     return 0;
